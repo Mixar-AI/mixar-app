a42684cafe78c9b3075c3804d375f5d343f9eec8
Chirag Singh <chirag.singh@mixar.app>
2026-08-19 16:26:07 +0530

    fix(paint): hide internal node groups from World/Properties socket menu (#1186)
    * fix(paint): hide internal node groups from World/Properties socket menu (Chirag Singh)
    
    Source-Branch: Mixar-AI/task/1077-layer-painting-group-names-popping-in-world-color-settings
    
    Private-SHA: 8f0dcd7a7f407a86fe20ac1c408635df8a0fe84f

--- src/source/blender/blenkernel/BKE_blender_version.h
+++ src/source/blender/blenkernel/BKE_blender_version.h
@@ -26,7 +26,7 @@
 #define BLENDER_VERSION_CYCLE release
 
 #define MIXAR_VERSION 303
-#define MIXAR_VERSION_PATCH 11
+#define MIXAR_VERSION_PATCH 12
 #define MIXAR_VERSION_CYCLE release
 
 /* Mixar file format version, stored in FileGlobal.
